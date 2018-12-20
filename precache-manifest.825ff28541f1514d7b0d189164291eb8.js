5dfd86a699084456c872a898ac57c9c175363a37
moody <dethanyel@gmail.com>
2018-12-20 08:59:37 -0500

    Updates

---
+++ precache-manifest.825ff28541f1514d7b0d189164291eb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2c275135a06121482d1",
+    "url": "/markdown-previewer/static/js/runtime~main.b2c27513.js"
+  },
+  {
+    "revision": "c290c44e9afbed2cbd68",
+    "url": "/markdown-previewer/static/js/main.c290c44e.chunk.js"
+  },
+  {
+    "revision": "477dc417c96b86c4ec88",
+    "url": "/markdown-previewer/static/js/1.477dc417.chunk.js"
+  },
+  {
+    "revision": "c290c44e9afbed2cbd68",
+    "url": "/markdown-previewer/static/css/main.44115b46.chunk.css"
+  },
+  {
+    "revision": "477dc417c96b86c4ec88",
+    "url": "/markdown-previewer/static/css/1.22973b9b.chunk.css"
+  },
+  {
+    "revision": "e1cc07a736c6c9a9d937cf09a4ec6592",
+    "url": "/markdown-previewer/index.html"
+  }
+];
